c5ec945e3edb5183df6a392b32be81563f03549c
MelArgumedo <alu.23130002@correo.itlalaguna.edu.mx>
2024-09-06 22:55:32 -0600

    Proyecto inicial

--- src/Principal.java
+++ src/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
         int num1 = Integer.parseInt(textField1.getText());
         int num2= Integer.parseInt(textField2.getText());
 
-
+//sumar
         btnSuma.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -39,6 +39,7 @@ int resultado = num1*num2;
 JOptionPane.showMessageDialog(null, "El resultado es: " + resultado);
             }
         });
+
 
         //division
         btnDivision.addActionListener(new ActionListener() {
